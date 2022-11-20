6ca54e651e529477e852cd925735b171a93ecc3c
李晏慈 <3a832005@gm.student.ncut.edu.tw>
2022-11-21 01:32:21 +0800

    CRUD-練習4:撰寫表單元件

--- resources/views/admin/posts/create.blade.php
+++ resources/views/admin/posts/create.blade.php
@@ -15,18 +15,28 @@
         </ul>
         <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
     </div>
-    <form>
+    <form action="{{ route('admin.posts.store') }}" method="POST" role="form">
+        @method('POST')
+        @csrf
         <div class="mb-3">
-            <label for="exampleFormControlInput1" class="form-label">文章標題</label>
-            <input type="text" class="form-control" placeholder="請輸入文章標題">
+            <label for="title" class="form-label">標題:</label>
+            <input id="title" name="title" class="form-control" value="{{ old('title') }}" placeholder="請輸入文章標題">
         </div>
         <div class="mb-3">
-            <label for="exampleFormControlTextarea1" class="form-label">文章內容</label>
-            <textarea class="form-control" rows="10" placeholder="請輸入文章內容"></textarea>
+            <label for="content" class="form-label">內容:</label>
+            <textarea id="content" name="content" class="form-control" rows="10"> {{ old('content') }}</textarea>
+        </div>
+        <div class="mb-3">
+            <label for=" is_feature " class="form-label">精選？</label>
+            <select id=" is_feature " name="is_feature" class="form-control">
+                <option value="0">否</option>
+                <option value="1">是</option>
+            </select>
         </div>
         <div class="d-grid gap-2 d-md-flex justify-content-md-end">
             <a class="btn btn-primary btn-sm" href="#">儲存</a>
         </div>
+
     </form>
 </div>
 @endsection
